7192ad0c33e661de6c65407e3f8713283aa9fd46
wuyangfan <1102042793@qq.com>
2023-03-24 17:19:56 +0800

    compiler-core:实现解析element类型

--- mini-vue/code/src/compiler-core/src/parse.ts
+++ mini-vue/code/src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast'
 
+const enum TagType {
+    Start,
+    End
+}
+
 export function baseParse(content) {
     const context = createParserContext(content)
     return createRoot(parseChildren(context))
@@ -9,8 +14,13 @@ export function baseParse(content) {
 function parseChildren(context) {
     const nodes = []
     let node: any
-    if (context.source.startsWith('{{')) {
+    const s = context.source
+    if (s.startsWith('{{')) {
         node = parseInterpolation(context)
+    } else if (s[0] === '<') {
+        if (/[a-z]/i.test(s[1])) {
+            node = parseElement(context)
+        }
     }
     nodes.push(node)
     return nodes
@@ -38,6 +48,26 @@ function parseInterpolation(context: any) {
         }
     }
 }
+
+function parseElement(context) {
+    const element = parseTag(context, TagType.Start)
+    parseTag(context, TagType.End)
+    return element
+}
+
+function parseTag(context: any, type: TagType) {
+    const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+    console.log(match);
+    const tag = match[1]
+    advanceBy(context, match[0].length)
+    advanceBy(context, 1)
+    if (type === TagType.End) return
+    return {
+        type: NodeTypes.ELEMENT,
+        tag
+    }
+}
+// 前进length步，代表当前处理了length个长度的字符了
 function advanceBy(context: any, length: number) {
     context.source = context.source.slice(length)
 }
