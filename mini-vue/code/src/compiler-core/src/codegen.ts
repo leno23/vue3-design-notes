788e48b64425f4f18d9ee293e9283fd68b8777c4
wuyangfan <1102042793@qq.com>
2023-03-27 17:14:37 +0800

    实现codegen:string类型

---
+++ mini-vue/code/src/compiler-core/src/codegen.ts
@@ -0,0 +1,31 @@
+
+export function generate(ast) {
+    const context = createCodegenContext()
+    const {push} = context
+    push('return ')
+    const functionName = 'render'
+    const args = ['_ctx', '_cache']
+    const signature = args.join(', ')
+    push(`function ${functionName}(${signature}){`) 
+
+    push(`\nreturn `)
+    const node = ast.codegenNode
+    genNode(node, context)
+    push('\n}')
+
+    return context
+}
+
+function createCodegenContext () {
+    const context = {
+        code:'',
+        push(source){
+            context.code +=source
+        }
+    }
+    return context
+}
+function genNode(node, context) {
+    const {push} = context
+   push(`'${node.content}'`)
+}
--- mini-vue/code/src/compiler-core/src/transform.ts
+++ mini-vue/code/src/compiler-core/src/transform.ts
@@ -1,8 +1,14 @@
 import { NodeTypes } from './ast';
 
-export function transform(root, options) {
+export function transform(root, options={}) {
     const context = createTransformContext(root, options)
     traverseNode(root, context)
+
+    createRootCodegen(root)
+}
+
+function createRootCodegen (root) {
+    root.codegenNode = root.children[0]
 }
 function createTransformContext(root, options) {
     const context = {
